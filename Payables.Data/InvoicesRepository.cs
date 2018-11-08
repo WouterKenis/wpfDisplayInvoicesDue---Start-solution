4bed057a858babdf49c346263be2f19f996a826d
wouterk3600 <wouterkenis1991@gmail.com>
2018-11-08 14:43:21 +0100

    while loop

--- Payables.Data/InvoicesRepository.cs
+++ Payables.Data/InvoicesRepository.cs
@@ -15,12 +15,50 @@ namespace Payables.Data
 
             //1) connection object -> PayablesDB.cs
             //2) connection open
+            //3) sqlcommand
+            //4) execute sqlcommand
             SqlConnection connection = PayablesDB.GetConnection();
+
+            string selectInvoices = "SELECT * FROM Invoices"; // kan nog een ander select statement achter
+                                                              // met ; na 'invoices'
+
+
+            //ExecuteReader geeft verschillende records
+            //ExecuteScalar geeft een getal
+            //ExecuteNonQuery past iets aan
+            SqlCommand selectInvoicesCommand = new SqlCommand(selectInvoices, connection);
+
+            SqlDataReader reader = null;
+            
             try
             {
                 connection.Open();
+                
+                reader = selectInvoicesCommand.ExecuteReader();
+
+                int indexInvoiceNumber = reader.GetOrdinal("InvoiceNumber");
+                int indexInvoiceDate = reader.GetOrdinal("InvoiceDate");
+                int indexInvoiceDueDate = reader.GetOrdinal("InvoiceDueDate");
+                int indexCreditTotal = reader.GetOrdinal("CreditTotal");
+                int indexPaymentTotal = reader.GetOrdinal("PaymentTotal");
+                int indexInvoiceTotal = reader.GetOrdinal("InvoiceTotal");
+
+                while (reader.Read())
+                {
+
+                    Invoice invoice = new Invoice();
+                    invoice.InvoiceNumber = reader[indexInvoiceNumber].ToString();
+                    // of reader.GetString(indexInvoiceNumber);
+                    // of reader["InvoiceNumber"].ToString()
+                    invoice.InvoiceDate = reader.GetDateTime(indexInvoiceDate);
+                    invoice.DueDate = reader.GetDateTime(indexInvoiceDueDate);
+                    invoice.CreditTotal = reader.GetDecimal(indexCreditTotal);
+                    invoice.PaymentTotal = reader.GetDecimal(indexPaymentTotal);
+                    invoice.InvoiceTotal = reader.GetDecimal(indexInvoiceTotal);
+
+                }
             }
-            catch (SqlException se)
+            catch (SqlException)
             {
 
             }
